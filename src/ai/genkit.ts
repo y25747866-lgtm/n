2af530b1aff0eb783487dcfafedab004ad69d9b4
yesh <y25747866@gmail.com>
2025-10-31 20:17:04 +0000

    Nothing is visible on this page and nothing can be searched for when pri

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,13 +1,12 @@
 
 import {genkit} from 'genkit';
-import { getApiKey } from '@/lib/firebase';
 import { googleAI } from '@genkit-ai/google-genai';
 
 export const ai = genkit({
   plugins: [
-    googleAI(async () => ({
-      apiKey: await getApiKey("GEMINI_API_KEY"),
-    })),
+    googleAI({
+      apiKey: process.env.GEMINI_API_KEY,
+    }),
   ],
   logLevel: 'debug',
   enableTracingAndMetrics: true,
