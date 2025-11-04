cc647e4d7794e51b64dac4d4dedd7fb3a4f20918
yesh <y25747866@gmail.com>
2025-11-04 07:23:22 +0000

    It might be a little problematic to load. Couldn't it be loading faster?

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { firebaseConfig } from '@/firebase/config';
@@ -32,3 +33,5 @@ export * from './non-blocking-updates';
 export * from './non-blocking-login';
 export * from './errors';
 export * from './error-emitter';
+export { useUser } from './provider';
+
