496e3b28fe362feff22b640457391e3ae517792a
yesh <y25747866@gmail.com>
2025-11-09 04:20:13 +0000

    import { initializeApp } from "firebase/app"; import { getFirestore } fr

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyD2tLb8ZBqbj8ezZbkkq4RjeVgPA2DmEfY",
   authDomain: "boss-69a16.firebaseapp.com",
   projectId: "boss-69a16",
-  storageBucket: "boss-69a16.appspot.com",
+  storageBucket: "boss-69a16.firebasestorage.app",
   messagingSenderId: "417923803661",
   appId: "1:417923803661:web:31193c1627e3c7da234344",
   measurementId: "G-S8WC0NND9D"
